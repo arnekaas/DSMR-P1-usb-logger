10ada2aa30957d3f465a51176aa4789f24fe1f90
Arne Kaas <arne.kaas@gmail.com>
2017-08-18 11:59:01 +0200

    added serial failure and baud=19200 try

--- p1logger.py
+++ p1logger.py
@@ -2,15 +2,16 @@
 # (c) 10-2012 - GJ - gratis te kopieren en te plakken
 # update Arne Kaas 10-08-2017
 
-versie = "2.0"
+version = "2.0"
 import sys
 import serial
+import signal, os
 
 ##############################################################################
 #Main program
 ##############################################################################
-print "USB DSMR P1 telegram reader, version "+  versie
-print "Control-C to exit"
+print ("USB DSMR P1 telegram reader, version "+  version)
+print ("Control-C to exit")
 
 #Set COM port config
 ser = serial.Serial()
@@ -21,39 +22,62 @@ ser.stopbits=serial.STOPBITS_ONE
 ser.xonxoff=0
 ser.rtscts=0
 ser.timeout=20
-ser.port="/dev/ttyUSB0"
+ser.port="/dev/ttyUSB0" #use on raspberrypi
+ser.port="/dev/tty.usbserial-P11KXDOA"  # use for testing on macs
 
-#Open COM port
-try:
-    ser.open()
-except:
-    try:
-        print ("Increased baudrate to 19200, retrying serial")
-        ser.baudrate = 19200
-        ser.open()
-    except:
-        sys.exit ("Fout bij het openen van %s. Aaaaarch."  % ser.name)
 
-#print(ser.name)         # check which port was really used
-
-#Initialize
-#p1_teller is mijn tellertje voor van 0 tot 20 te tellen
+#Initialize empty telegram
 telegram = ''
 
-while True:
-    p1_raw = ser.readline()
-    p1_str=str(p1_raw)
-    p1_line=p1_str.strip()
-# als je alles wil zien moet je de volgende line uncommenten
-    print (p1_line)
-    telegram += str(p1_line) + '\n'
-    if p1_line == '!':
-	#Close port and show status
-	try:
-	     ser.close()
-	except:	
-	     sys.exit ("Oops %s. Programma afgebroken. Kon de seriele poort niet sluiten." % ser.name )
-	break
+#set timeonut
+def handler(signum, frame):
+    ser.close()
+    print("Error handler called with errornum: ", signum)
+    raise OSError("No proper DSMR P1 telegram recieved.")
+
+def read_DSMR_telegram():
+    #Open COM port
+    try:
+        ser.open()
+    except:
+        sys.exit ("Coul not open serial '%s'. Aaaaarch."  % ser.name)
+    
+	# Set the signal handler and a 15-second alarm
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(5)
+    
+    while True:
+        #proces serial line to string
+        p1_raw = ser.readline()
+        p1_str=str(p1_raw)
+        p1_line=p1_str.strip()
+    
+        #print for debugging purpose
+        print (p1_line) 
+    
+        #add line to telegram
+        telegram += str(p1_line) + '\n'
+    
+        # stop reading if '!' was found (DSMR telegram ends with it.)
+        if p1_line == '!':
+    	    #Close port and show status
+    	    try:
+    	        ser.close()
+    	    except:	
+    	        sys.exit ("Oops %s. Program aborted. Could not close serial port" % ser.name )
+    	    break
+
+    signal.alarm(0)          # Disable the alarm
+
+try:
+	read_DSMR_telegram()
+except:
+    print ("Increased baudrate to 19200, retrying serial")    
+    try:
+        ser.baudrate = 19200
+        read_DSMR_telegram()
+    except:
+        print("19200 also failed")
 
 text_file = open("logs/lastP1read.txt", "w")
 text_file.write(telegram)
